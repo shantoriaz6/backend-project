08db0eb9ee93bf0596f432c00c91d36ca8074244
shantoriaz6 <shantoriaz2001@gmail.com>
2025-08-25 20:46:33 +0600

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asynHandler = (requestHandler) => {
+    (req, res, next) => {
+        promise.resolve(requestHandler(req, res, next)).catch((err) => next (err))
+    }
+}
+
+
+
+
+
+export {asynHandler}
+
+/*const asynHandler = (fn) => async (req, res, next) => {
+    try {
+       await fn(req, res, next)
+    } catch (error) {
+     res.status(error.code || 500) .json({
+        success: false,
+        message: err.message
+     })  
+    }
+}*/
