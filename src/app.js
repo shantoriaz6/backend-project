2a41e449716a4b52b5fc973ea49412f718e49624
shantoriaz6 <shantoriaz2001@gmail.com>
2025-10-17 22:26:28 +0600

    feat: add tweet management functionalities; implement routes and controller methods for creating, fetching, updating, and deleting tweets

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ import likeRouter from './routes/like.routes.js';
 
 import commentRouter from './routes/comment.routes.js'
 
+import tweetRouter from './routes/tweet.routes.js';
+
 
 //routes decleration
 app.use("/api/v1/users", userRouter)
@@ -38,8 +40,12 @@ app.use("/api/v1/playlists", playlistRouter)
 
 app.use("/api/v1/subscriptions", subscriptionRouter)
 
-//app.use("/api/v1/likes", likeRouter)
+
 app.use("/api/v1/likes", likeRouter);
+
+
 app.use("/api/v1/comments", commentRouter)
+
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app }
